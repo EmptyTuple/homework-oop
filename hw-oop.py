40ab9c5085f54b377462050dff879a1762357815
EmptyTuple <kotorov@outlook.com>
2022-03-12 23:32:48 +0700

    __str__ reassigned for all classes

--- hw-oop.py
+++ hw-oop.py
@@ -21,6 +21,30 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.student_grades = {}
+    
+    def __str__(self):
+        average_grade = 0
+        lecturer_bio = f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {average_grade}'
+        return lecturer_bio 
+
+class Reviewer(Mentor):
+    
+    def __str__(self):
+        reviewer_bio = f'Имя: {self.name} \nФамилия: {self.surname}'
+        return reviewer_bio 
+
+lecturer1 = Lecturer('Иван', 'Иванов')
+print(lecturer1)
+
+reviewer1 = Reviewer('Петр', 'Петров')
+print(reviewer1)
+
+
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
